eefefc38489b9bdf29beaff2ba22979015ddaf76
bintang4974 <bintangramadhan4974@gmail.com>
2024-07-03 22:57:16 +0700

    adding middleware user & logout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,6 +38,14 @@
                 <h4>Silahkan Login</h4>
             </div>
             <div class="section mt-1 mb-5">
+                @php
+                    $message = Session::get('warning');
+                @endphp
+                @if (Session::get('warning'))
+                    <div class="alert alert-outline-warning">
+                        {{ $message }}
+                    </div>
+                @endif
                 <form action="/processlogin" method="POST">
                     @csrf
                     <div class="form-group boxed">
