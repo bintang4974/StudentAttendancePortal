3811501c7d67b1c573a19a32258d360692aefe25
bintang4974 <bintangramadhan4974@gmail.com>
2024-07-11 10:42:30 +0700

    fixing attendance photo in & out

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        $today = date('Y-m-d');
+        $student_id = Auth::guard('student')->user()->id;
+        $attendanceToday = DB::table('attendances')->where('student_id', $student_id)->where('date', $today)->first();
+        return view('dashboard.dashboard', compact('attendanceToday'));
     }
 }
