eeef643d24b70c083e240c1c5d9226d830ce1239
bintang4974 <bintangramadhan4974@gmail.com>
2024-07-30 11:21:56 +0700

    search & pagination dashboard admin

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // $student = DB::table('students')
-        //     ->orderBy('name')
-        //     ->join('departments', 'student.department_id', '=', 'departments.id')
-        //     ->get();
-        $student = Student::paginate(1);
 
-        return view('student.index', compact('student'));
+        $query = Student::query();
+        $query->select('students.*', 'departments.name as name_department');
+        $query->join('departments', 'students.department_id', '=', 'departments.id',);
+        $query->orderBy('name');
+        if (!empty($request->name_student)) {
+            $query->where('students.name', 'like', '%' . $request->name_student . '%');
+        }
+        if (!empty($request->name_dept)) {
+            $query->where('departments.name', $request->name_dept);
+        }
+        $student = $query->paginate(1);
+        $department = Department::all();
+
+        return view('student.index', compact('student', 'department'));
     }
 }
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -21,54 +21,98 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Name</th>
-                                        <th>NIM</th>
-                                        <th>Email</th>
-                                        <th>Phone</th>
-                                        <th>University</th>
-                                        <th>Gender</th>
-                                        <th>City</th>
-                                        <th>Address</th>
-                                        <th>Photo</th>
-                                        <th>Department</th>
-                                        <th>Mentor</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($student as $item)
-                                        @php
-                                            $path = Storage::url('uploads/student/' . $item->photo);
-                                        @endphp
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $item->name }}</td>
-                                            <td>{{ $item->nim }}</td>
-                                            <td>{{ $item->email }}</td>
-                                            <td>{{ $item->phone }}</td>
-                                            <td>{{ $item->university }}</td>
-                                            <td>{{ $item->gender }}</td>
-                                            <td>{{ $item->city }}</td>
-                                            <td>{{ $item->address }}</td>
-                                            <td>
-                                                @if (empty($item->photo))
-                                                    <img src="{{ asset('logo/no_photo.png') }}" class="avatar">
-                                                @else
-                                                    <img src="{{ url($path) }}" class="avatar">
-                                                @endif
-                                            </td>
-                                            <td>{{ $item->department->name }}</td>
-                                            <td>{{ $item->mentor->name }}</td>
-                                            <td>{{ $item->mentor_id }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            {{ $student->links() }}
+                            <div class="row">
+                                <div class="col-12">
+                                    <form action="/student" method="GET">
+                                        <div class="row">
+                                            <div class="col-6">
+                                                <div class="form-group">
+                                                    <input type="text" name="name_student" id="name_student"
+                                                        class="form-control" placeholder="Nama Karyawan"
+                                                        value="{{ Request('name_student') }}">
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+                                                <select name="name_dept" id="name_dept" class="form-select">
+                                                    <option value="">Department</option>
+                                                    @foreach ($department as $item)
+                                                        <option {{ Request('name_dept') == $item->name ? 'selected' : '' }}
+                                                            value="{{ $item->name }}">{{ $item->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="col-2">
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                                                            height="24" viewBox="0 0 24 24" fill="none"
+                                                            stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            class="icon icon-tabler icons-tabler-outline icon-tabler-search">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                                                            <path d="M21 21l-6 -6" />
+                                                        </svg>
+                                                        Search
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>NIM</th>
+                                                <th>Email</th>
+                                                <th>Phone</th>
+                                                <th>University</th>
+                                                <th>Gender</th>
+                                                <th>City</th>
+                                                <th>Address</th>
+                                                <th>Photo</th>
+                                                <th>Department</th>
+                                                <th>Mentor</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($student as $item)
+                                                @php
+                                                    $path = Storage::url('uploads/student/' . $item->photo);
+                                                @endphp
+                                                <tr>
+                                                    <td>{{ $loop->iteration + $student->firstItem() - 1 }}</td>
+                                                    <td>{{ $item->name }}</td>
+                                                    <td>{{ $item->nim }}</td>
+                                                    <td>{{ $item->email }}</td>
+                                                    <td>{{ $item->phone }}</td>
+                                                    <td>{{ $item->university }}</td>
+                                                    <td>{{ $item->gender }}</td>
+                                                    <td>{{ $item->city }}</td>
+                                                    <td>{{ $item->address }}</td>
+                                                    <td>
+                                                        @if (empty($item->photo))
+                                                            <img src="{{ asset('logo/no_photo.png') }}" class="avatar">
+                                                        @else
+                                                            <img src="{{ url($path) }}" class="avatar">
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $item->name_department }}</td>
+                                                    <td>{{ $item->mentor->name }}</td>
+                                                    <td>{{ $item->mentor_id }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                    {{ $student->links('vendor.pagination.bootstrap-5') }}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
